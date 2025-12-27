f251ae56d11b791fde57a05cc223ce8f3faca928
Emo0o0 <emil.kolev1102@gmail.com>
2025-12-27 22:22:34 +0200

    Missing exception file

---
+++ src/main/java/com/example/core/exception/ChatRoomAddException.java
@@ -0,0 +1,7 @@
+package com.example.core.exception;
+
+public class ChatRoomAddException extends RuntimeException{
+    public ChatRoomAddException(String message) {
+        super(message);
+    }
+}
